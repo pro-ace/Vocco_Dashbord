9cb9d4ebae45196d29652616f35787e85c99ae10
IronHeart0225 <ironheart0225@gmail.com>
2022-08-25 21:37:50 +0900

    fix errors on console

--- src/app/modules/apps/user-management/users-list/table/columns/UserInfoCell.tsx
+++ src/app/modules/apps/user-management/users-list/table/columns/UserInfoCell.tsx
@@ -1,44 +1,38 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import clsx from 'clsx'
-import {FC} from 'react'
+import { FC } from 'react'
 import { Link } from 'react-router-dom'
-import {User} from '../../core/_models'
+import { User } from '../../core/_models'
 
 type Props = {
   user: User,
 }
 
-const UserInfoCell: FC<Props> = ({user}) => (
-  <div className='d-flex align-items-center'>
+const UserInfoCell: FC<Props> = ({ user }) => (
+  <Link to={`profile/${user.id}`} className='d-flex align-items-center'>
     {/* begin:: Avatar */}
     <div className='symbol symbol-circle symbol-50px overflow-hidden me-3'>
-      <a href='#'>
-        {user.avatar ? (
-          <div className='symbol-label'>
-            <Link to={`profile/${user.id}`}>
-              <img src={`${user.avatar.url}`} alt={user.name} className='w-100' />
-            </Link>
-          </div>
-        ) : (
-          <div
-            className={clsx(
-              'symbol-label fs-3',
-              `bg-light-${user.initials?.state}`,
-              `text-${user.initials?.state}`
-            )}
-          >
-            {user.initials?.label}
-          </div>
-        )}
-      </a>
+      {user.avatar ? (
+        <div className='symbol-label'>
+          <img src={`${user.avatar.url}`} alt={user.name} className='w-100' />
+        </div>
+      ) : (
+        <div
+          className={clsx(
+            'symbol-label fs-3',
+            `bg-light-${user.initials?.state}`,
+            `text-${user.initials?.state}`
+          )}
+        >
+          {user.initials?.label}
+        </div>
+      )}
     </div>
     <div className='d-flex flex-column'>
-      <Link to={`profile/${user.id}`}>
-        {user.name}
-      </Link>
+      {user.name}
       <span>{user.email}</span>
     </div>
-  </div>
+  </Link>
 )
 
-export {UserInfoCell}
+export { UserInfoCell }
